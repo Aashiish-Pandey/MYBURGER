503e3a02b5dba250177d9e63162eeb126e3bbf9d
Aashiish-Pandey <ash006pandey@gmail.com>
2021-03-03 14:01:34 +0530

    adding the price to order summery

--- src/components/layout/Burger/OrderSummary/OrderSummary.js
+++ src/components/layout/Burger/OrderSummary/OrderSummary.js
@@ -16,10 +16,11 @@ const orderSummary = (props) => {
 
   return (
     <Aux>
-      
+
       <h3>Your Order</h3>
       <p> A delicious burger with the following ingredients:</p>
       <ul>{ingredientSummary}</ul>
+      <p><strong>Total price : {props.price.toFixed(2)}</strong></p>
       <p>Continue to Checkout</p>
       <Button btnType = "Danger" clicked = {props.purchaseCancelled}></Button>
       <Button btnType = "Success" clicked = {props.purchaseContinued}></Button>
